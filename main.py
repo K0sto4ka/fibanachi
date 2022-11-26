c13f47d51d50675a6d5f3d184aab7581d3c76500
kosto4ka <krivcovkonstantin48@gmail.com>
2022-11-26 11:41:03 +0300

    111111

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int(input('Ввод: '))
+
+def F(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return F(n-1)+F(n-2)
+
+
+print('Вывод:', F(n))
+input()
